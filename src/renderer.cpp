622091e4c9593b8b70c88fa4b561bd0ebdea8cd6
Niklas Mäckle <niklas.maeckle@gmail.com>
2025-06-06 17:34:13 +0200

    Finished slicing greedy meshing algorithm

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -220,7 +220,7 @@ void Renderer::init(uint16_t size_x, uint16_t size_y, bool enable_vsync, bool en
     
     // set up vertex data (and buffer(s)) and configure vertex attributes
     //-----------------------------------------------------------------
-    compute_scene.load("../res/vox/cube.vox", slicing);
+    compute_scene.load("../res/vox/45mins.vox", slicing);
     //chunk.generate_buffers();
     //quad.generate_buffers();
 
